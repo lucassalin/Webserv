f4362959df88df599193751f9aac36380655d518
Lucas <lucas@MacBook-Air-de-Lucas.local>
2023-04-07 15:32:19 +0200

    Not validate

--- srcs/ConfigFile.cpp
+++ srcs/ConfigFile.cpp
@@ -6,9 +6,86 @@
 /*   By: lsalin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 14:34:29 by lsalin            #+#    #+#             */
-/*   Updated: 2023/04/07 14:34:44 by lsalin           ###   ########.fr       */
+/*   Updated: 2023/04/07 15:12:00 by lsalin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ConfigFile.hpp"
 
+ConfigFile::ConfigFile() : _size(0) {}
+
+ConfigFile::ConfigFile(std::string const path) : _path(path), _size(0) {}
+
+ConfigFile::~ConfigFile() {}
+
+/**
+	@brief Détermine le type du chemin fourni en paramètre
+	@brief stat : fonction qui remplit la structure stat avec les infos sur un dossier
+
+	@return (1) si c'est un fichier, (2) si c'est un dossier et (3) si c'est ni l'un ni l'autre
+	@return (-1) si échec
+ */
+
+int	ConfigFile::getTypePath(std::string const path)
+{
+	struct stat	buffer;
+	int			result;
+
+	result = stat(path.c_str(), &buffer);
+
+	// Utilisation du "ET logique" (&) pour tester 
+	// si le bit en question est activé ou non
+
+	if (result == 0)
+	{
+		if (buffer.st_mode & S_IFREG)
+			return (1);
+		else if (buffer.st_mode & S_IFDIR)
+			return (2);
+		else
+			return (3);
+	}
+	else
+		return (-1);
+}
+
+// Retourne (0) si le fichier existe et que l'user a les permissions nécessaires 
+// en fonction d'un certain mode
+// Sinon (-1)
+
+int	ConfigFile::checkFile(std::string const path, int mode)
+{
+	return (access(path.c_str(), mode));
+}
+
+/**
+	@brief Check un fichier existe et si l'user a les permissions nécessaires pour le lire
+	
+ 	@param path : chemin du fichier
+	@param index : nom du fichier
+
+	@return (0) si le fichier existe et que l'user a les permissions, (-1) sinon
+ */
+
+int	ConfigFile::isFileExistAndReadable(std::string const path, std::string const index)
+{
+	// Si index est un fichier et a les permissions de lecture (mode 4)
+	if (getTypePath(index) == 1 && checkFile(index, 4) == 0)
+		return (0);
+
+	// path + index = chemin complet du fichier par concaténation
+	if (getTypePath(path + index) == 1 && checkFile(path + index, 4) == 0)
+		return (0);
+
+	return (-1);
+}
+
+std::string	ConfigFile::getPath()
+{
+	return (this->_path);
+}
+
+int	ConfigFile::getSize()
+{
+	return (this->_size);
+}
--- srcs/Location.cpp
+++ srcs/Location.cpp
@@ -6,7 +6,7 @@
 /*   By: lsalin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 15:28:18 by lsalin            #+#    #+#             */
-/*   Updated: 2023/04/07 14:33:31 by lsalin           ###   ########.fr       */
+/*   Updated: 2023/04/07 15:31:57 by lsalin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,3 +83,191 @@ void	Location::setRootLocation(std::string parametr)
 	this->_root = parametr;
 }
 
+// Définit les méthodes HTTP autorisées pour une instance de Localisation
+
+void	Location::setMethods(std::vector<std::string> methods)
+{
+	this->_methods[0] = 0;
+	this->_methods[1] = 0;
+	this->_methods[2] = 0;
+	this->_methods[3] = 0;
+	this->_methods[4] = 0;
+
+	// Définit a 1 la méthode passée en paramètre
+	for (size_t i = 0; i < methods.size(); i++)
+	{
+		if (methods[i] == "GET")
+			this->_methods[0] = 1;
+		else if (methods[i] == "POST")
+			this->_methods[1] = 1;
+		else if (methods[i] == "DELETE")
+			this->_methods[2] = 1;
+		else if (methods[i] == "PUT")
+			this->_methods[3] = 1;
+		else if (methods[i] == "HEAD")
+			this->_methods[4] = 1;
+		else
+			throw ServerConfig::ErrorException("Allow method not supported " + methods[i]);
+	}
+}
+
+// Définit la valeur de _autoindex
+// En fonction que l'auto-indexation est validée ou non
+// 
+
+void	Location::setAutoindex(std::string parametr)
+{
+	// si on --> _autoindex = true
+	// si off --> _autoindex = false
+	if (parametr == "on" || parametr == "off")
+		this->_autoindex = (parametr == "on");
+	else
+		throw ServerConfig::ErrorException("Wrong autoindex");
+}
+
+void	Location::setIndexLocation(std::string parametr)
+{
+	this->_index = parametr;
+}
+
+void	Location::setReturn(std::string parametr)
+{
+	this->_return = parametr;
+}
+
+void	Location::setAlias(std::string parametr)
+{
+	this->_alias = parametr;
+}
+
+void	Location::setCgiPath(std::vector<std::string> path)
+{
+	this->_cgi_path = path;
+}
+
+void	Location::setCgiExtension(std::vector<std::string> extension)
+{
+	this->_cgi_ext = extension;
+}
+
+// Définit la taille maximale du body d'une requête acceptée pour cette location
+// Si paramtr = 100, la taille max du body est de 100 octets
+
+void	Location::setMaxBodySize(std::string parametr)
+{
+	unsigned long body_size = 0;
+
+	// Check que paramtr contient bien des chiffres
+	for (size_t i = 0; i < parametr.length(); i++)
+	{
+		if (parametr[i] < '0' || parametr[i] > '9')
+			throw ServerConfig::ErrorException("Wrong syntax: client_max_body_size");
+	}
+
+	// Convertit la string en un unsigned int
+	if (!ft_stoi(parametr))
+		throw ServerConfig::ErrorException("Wrong syntax: client_max_body_size");
+		
+	body_size = ft_stoi(parametr);
+	this->_client_max_body_size = body_size;
+}
+
+void Location::setMaxBodySize(unsigned long parametr)
+{
+	this->_client_max_body_size = parametr;
+}
+
+const	std::string &Location::getPath() const
+{
+	return (this->_path);
+}
+
+const	std::string &Location::getRootLocation() const
+{
+	return (this->_root);
+}
+
+const	std::string &Location::getIndexLocation() const
+{
+	return (this->_index);
+}
+
+const	std::vector<short> &Location::getMethods() const
+{
+	return (this->_methods);
+}
+
+const	std::vector<std::string> &Location::getCgiPath() const
+{
+	return (this->_cgi_path);
+}
+
+const	std::vector<std::string> &Location::getCgiExtension() const
+{
+	return (this->_cgi_ext);
+}
+
+const	bool &Location::getAutoindex() const
+{
+	return (this->_autoindex);
+}
+
+const	std::string &Location::getReturn() const
+{
+	return (this->_return);
+}
+
+const	std::string &Location::getAlias() const
+{
+	return (this->_alias);
+}
+
+const	std::map<std::string, std::string> &Location::getExtensionPath() const
+{
+	return (this->_ext_path);
+}
+
+const	unsigned long &Location::getMaxBodySize() const
+{
+	return (this->_client_max_body_size);
+}
+
+// Retourne une string représentant les méthodes HTTP autorisées pour la location
+
+std::string Location::getPrintMethods() const
+{
+	std::string	res;
+	
+	if (_methods[4])
+		res.insert(0, "HEAD");
+
+	if (_methods[3])
+	{
+		if (!res.empty())
+			res.insert(0, ", ");
+		res.insert(0, "PUT");
+	}
+
+	if (_methods[2])
+	{
+		if (!res.empty())
+			res.insert(0, ", ");
+		res.insert(0, "DELETE");
+	}
+
+	if (_methods[1])
+	{
+		if (!res.empty())
+			res.insert(0, ", ");
+		res.insert(0, "POST");
+	}
+
+	if (_methods[0])
+	{
+		if (!res.empty())
+			res.insert(0, ", ");
+		res.insert(0, "GET");
+	}
+
+	return (res);
+}
