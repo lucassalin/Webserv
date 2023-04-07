937d66966d15735ffcd2b3ce34da6dd6afd41530
Ontiimeyes <lucassalinroge@gmail.com>
2023-04-07 11:16:29 +0200

    Gitgnore added

--- includes/CgiHandler.hpp
+++ includes/CgiHandler.hpp
@@ -6,7 +6,7 @@
 /*   By: lsalin <lsalin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 11:00:47 by lsalin            #+#    #+#             */
-/*   Updated: 2023/04/07 11:00:51 by lsalin           ###   ########.fr       */
+/*   Updated: 2023/04/07 11:15:24 by lsalin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@
 
 class HttpRequest;
 
-class CgiHandler {
+// Gere les appels aux scripts CGI
+
+class CgiHandler
+{
 	private:
 		std::map<std::string, std::string>	_env;
-		char**								_ch_env;
-		char**								_argv;
+		char								**_ch_env;
+		char								**_argv;
 		int									_exit_status;
 		std::string							_cgi_path;
 		pid_t								_cgi_pid;
@@ -34,29 +37,30 @@ class CgiHandler {
 		CgiHandler(std::string path);
 		~CgiHandler();
 		CgiHandler(CgiHandler const &other);
-		CgiHandler &operator=(CgiHandler const &rhs);
+		CgiHandler	&operator=(CgiHandler const &rhs);
 
-		void initEnv(HttpRequest& req, const std::vector<Location>::iterator it_loc);
-		void initEnvCgi(HttpRequest& req, const std::vector<Location>::iterator it_loc);
-		void execute(short &error_code);
-		void sendHeaderBody(int &pipe_out, int &fd, std::string &);
-		void fixHeader(std::string &header);
-		void clear();
+		void		initEnv(HttpRequest& req, const std::vector<Location>::iterator it_loc);
+		void		initEnvCgi(HttpRequest& req, const std::vector<Location>::iterator it_loc);
+		void 		execute(short &error_code);
+		void 		sendHeaderBody(int &pipe_out, int &fd, std::string &);
+		void 		fixHeader(std::string &header);
+		void		clear();
+		
 		std::string setCookie(const std::string& str);
 
-		void setCgiPid(pid_t cgi_pid);
-		void setCgiPath(const std::string &cgi_path);
+		void 		setCgiPid(pid_t cgi_pid);
+		void		setCgiPath(const std::string &cgi_path);
 
-		const std::map<std::string, std::string> &getEnv() const;
-		const pid_t &getCgiPid() const;
-		const std::string &getCgiPath() const;
+		const std::map<std::string, std::string>	&getEnv() const;
+		const pid_t									&getCgiPid() const;
+		const std::string							&getCgiPath() const;
 
 		std::string	getAfter(const std::string& path, char delim);
 		std::string	getBefore(const std::string& path, char delim);
 		std::string	getPathInfo(std::string& path, std::vector<std::string> extensions);
-		int	countCookies(const std::string& str);
-		int findStart(const std::string path, const std::string delim);
-		std::string decode(std::string &path);
+		int			countCookies(const std::string& str);
+		int			findStart(const std::string path, const std::string delim);
+		std::string	decode(std::string &path);
 };
 
 #endif
