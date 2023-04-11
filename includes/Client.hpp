c44b9ed2205cd463e6f6bd0a9a12bec76e82e30b
Lucas <lucas@MacBook-Air-de-Lucas.local>
2023-04-11 15:04:03 +0200

    Starting Client

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Client.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lsalin <lsalin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lsalin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 14:11:05 by lsalin            #+#    #+#             */
-/*   Updated: 2023/04/10 14:11:06 by lsalin           ###   ########.fr       */
+/*   Updated: 2023/04/11 15:03:30 by lsalin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,38 +18,40 @@
 #include "Response.hpp"
 
 /**
-     Client
+	 Client
    - Stores all information related to the client such as socket and address
-     along with request and response objects. each Client also have an object of the server it's conntected to.
+	 along with request and response objects. each Client also have an object of the server it's conntected to.
 **/
+
 class Client
 {
-    public:
-        Client();
-        Client(const Client &other);
-        Client(ServerConfig &);
-		    Client &operator=(const Client & rhs);
-        ~Client();
+	public:
+		Client();
+		Client(const Client &other);
+		Client(ServerConfig &);
+			Client &operator=(const Client & rhs);
+		~Client();
 
-        const int                 &getSocket() const;
-        const struct sockaddr_in  &getAddress() const;
-        const HttpRequest         &getRequest() const;
-        const time_t              &getLastTime() const;
+		const int                 &getSocket() const;
+		const struct sockaddr_in  &getAddress() const;
+		const HttpRequest         &getRequest() const;
+		const time_t              &getLastTime() const;
 
-        void                setSocket(int &);
-        void                setAddress(sockaddr_in &);
-        void                setServer(ServerConfig &);
-        void                buildResponse();
-        void                updateTime();
+		void                setSocket(int &);
+		void                setAddress(sockaddr_in &);
+		void                setServer(ServerConfig &);
+		void                buildResponse();
+		void                updateTime();
 
-        void                clearClient();
-        Response            response;
-        HttpRequest         request;
-        ServerConfig        server;
-    private:
-        int                 _client_socket;
-        struct sockaddr_in  _client_address;
-        time_t              _last_msg_time;
+		void                clearClient();
+		Response            response;
+		HttpRequest         request;
+		ServerConfig        server;
+
+	private:
+		int                 _client_socket;
+		struct sockaddr_in  _client_address;
+		time_t              _last_msg_time;
 };
 
 
